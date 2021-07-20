ef6790d605e7c7becdc86ec07ca44a47ed50593d
Alex Kramer <alex@keicreations.nl>
2021-07-20 13:49:42 +0200

    add eslint-config-prettier-vue, use prettier-vue and some cleanup

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 module.exports = {
     extends: [
-        'eslint-config-hardcore/base',
+        'eslint-config-hardcore/index',
         'eslint-config-hardcore/ts-for-js',
         'plugin:vue/recommended',
         '@vue/typescript',
-        'plugin:prettier/recommended'
+        './vue-specific-rules.js',
+        'plugin:prettier-vue/recommended',
+        'prettier'
     ],
     plugins: [
         "no-loops",
@@ -15,38 +17,9 @@ module.exports = {
         browser: true,
     },
     rules: {
-        "vue/html-indent": ["error", 4],
-        "vue/padding-line-between-blocks": ["error", "always"],
-        "vue/attributes-order": "error",
-        "vue/component-tags-order": "error",
-        "vue/no-lone-template": "error",
-        "vue/no-multiple-slot-args": "error",
-        "vue/no-v-html": "error",
-        "vue/order-in-components": "error",
-        "vue/this-in-template": "error",
-        "vue/attribute-hyphenation": "error",
-        "vue/component-definition-name-casing": "error",
-        "vue/html-closing-bracket-newline": "error",
-        "vue/html-closing-bracket-spacing": "error",
-        "vue/html-end-tags": "error",
-        "vue/html-quotes": "error",
-        "vue/html-self-closing": "error",
-        "vue/max-attributes-per-line": "error",
-        "vue/multiline-html-element-content-newline": "error",
-        "vue/mustache-interpolation-spacing": "error",
-        "vue/no-multi-spaces": "error",
-        "vue/no-spaces-around-equal-signs-in-attribute": "error",
-        "vue/no-template-shadow": "error",
-        "vue/one-component-per-file": "error",
-        "vue/prop-name-casing": "error",
-        "vue/require-default-prop": "error",
-        "vue/require-prop-types": "error",
-        "vue/singleline-html-element-content-newline": "error",
-        "vue/v-bind-style": "error",
-        "vue/v-on-style": "error",
-        "vue/v-slot-style": "error",
+        "prettier/prettier": "off",
 
-        "prettier/prettier": ["error", {
+        "prettier-vue/prettier": ["error", {
             semi: false,
             singleQuote: true,
             tabWidth: 4,
